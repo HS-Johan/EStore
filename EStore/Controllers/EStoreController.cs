64eabc2414935ac0fcc89bd4ca54847c4c28a1de
Hasan Shahriar <johan.cse5.bu@gmail.com>
2025-01-16 07:02:49 +0600

    AddToCart_16Jan24_v1

---
+++ EStore/Controllers/EStoreController.cs
@@ -0,0 +1,33 @@
+﻿using EStore.Data;
+using EStore.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EStore.Controllers
+{
+    public class EStoreController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EStoreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var allproducts = _context.Product.Take(10).ToList();
+
+            var viewmodel = new HomePageVM();
+
+            viewmodel.Product = allproducts;
+
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart([FromBody] int productId)
+        {
+            return Ok();
+        }
+    }
+}
--- EStore/ViewModel/HomePageVM.cs
+++ EStore/ViewModel/HomePageVM.cs
@@ -4,8 +4,10 @@ namespace EStore.ViewModel
 {
     public class HomePageVM
     {
-        public IEnumerable<Product> Product { get; set; }
+        public IEnumerable<Product>? Product { get; set; }
 
-        public IEnumerable<Label> label { get; set; }
+        public IEnumerable<Label>? label { get; set; }
+
+        public IEnumerable<LookUp>? LookUp { get; set; }
     }
 }
